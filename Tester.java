8606d7294ad728d272ef6a006c5b29253d02f38a
ryan-li20 <rli20@stuy.edu>
2020-11-22 21:38:37 -0500

    tested sm

--- Tester.java
+++ Tester.java
@@ -1,6 +1,25 @@
 public class Tester{
   public static void main(String[] args){
-    equalsTest();
+    System.out.println(gcd(100, 10));
+  }
+
+  public static int gcd(int x, int y){
+    /*use euclids method or a better one*/
+    //http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
+    int r = 1;
+    int a = Math.max(Math.abs(x), Math.abs(y));
+    int b = Math.min(Math.abs(x), Math.abs(y));
+    boolean zero = false;
+    while (!zero){
+      int remainder = a % b;
+      if(remainder == 0){
+        zero = false;
+        break;
+      }
+      a = b;
+      b = remainder;
+    }
+    return b;
   }
 
   public static void equalsTest(){
@@ -18,4 +37,15 @@ public class Tester{
     System.out.println(a.equals(e));
     System.out.println(!a.equals(f));
   }
+
+  public static void sum(){
+    RealNumber a = new RealNumber(8.0);
+    RealNumber b = new RealNumber(16.942);
+    RealNumber c = new RealNumber(-14.36);
+    RealNumber d = new RealNumber(Math.sqrt(3));
+    System.out.println();
+  }
+
+
+
 }
